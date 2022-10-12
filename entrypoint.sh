87b7a996efe2175583f9dca1c0e42d34dc5e0c70
Johannes Köster <johannes.koester@uni-due.de>
2022-10-12 14:31:48 +0200

    dbg

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ shopt -s extglob
 cd $GITHUB_WORKSPACE
 
 source activate snakedeploy
+echo "snakedeploy $1 $2"
+ls -l
+pip install git+https://github.com/snakemake/snakedeploy.git@dbg/env-update
 eval "snakedeploy $1 $2"
