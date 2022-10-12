e39604ad66bab363563c41d1d090ba9259a9614f
Johannes Köster <johannes.koester@uni-due.de>
2022-10-12 14:04:07 +0200

    try eval

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,5 @@ shopt -s extglob
 
 cd $GITHUB_WORKSPACE
 
-echo "activating snakedeploy env"
-
 source activate snakedeploy
-echo "running snakedeploy with $1 $2"
-pip install git+https://github.com/snakemake/snakedeploy.git@dbg/env-update
-snakedeploy $1 $2
-snakedeploy --help
+eval "snakedeploy $1 $2"
